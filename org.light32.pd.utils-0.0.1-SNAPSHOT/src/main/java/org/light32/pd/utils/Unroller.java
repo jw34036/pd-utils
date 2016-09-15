01087a00b547383728208f31d44e0739533a103f
Josh Whitt <jw34036@gmail.com>
2016-09-15 00:01:04 -0500

    Added Unroller

---
+++ org.light32.pd.utils-0.0.1-SNAPSHOT/src/main/java/org/light32/pd/utils/Unroller.java
@@ -0,0 +1,176 @@
+package org.light32.pd.utils;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * provides unrolled loops.  An unrolled loop is a loop that processes multiple entries of an array or list at once.
+ * Since the point of this technique is efficiency by use of a loop block that avoids dynamic code in each iteration,
+ * this requires some duplication.
+ * <p>
+ * Each xN method will process the List N times per iteration by calling the supplied Consumer.
+ * It is up to the user to avoid calling a method that will leave remainders - doing so will
+ * throw exceptions.  the gfac() utility method
+ *
+ * @author jwhitt 9/14/16
+ */
+public class Unroller {
+
+    public static <R> void x2(List<R> list, final Consumer<R> action) {
+        for (int i = 0; i < list.size(); i += 2) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+        }
+    }
+
+    public static <R> void x3(List<R> list, final Consumer<R> action) {
+
+        for (int i = 0; i < list.size(); i += 3) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+        }
+    }
+
+    public static <R> void x4(List<R> list, final Consumer<R> action) {
+
+        for (int i = 0; i < list.size(); i += 4) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+            if ((i + 3) < list.size()) action.accept(list.get(i + 3));
+        }
+    }
+
+    public static <R> void x5(List<R> list, final Consumer<R> action) {
+
+        for (int i = 0; i < list.size(); i += 5) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+            if ((i + 3) < list.size()) action.accept(list.get(i + 3));
+            if ((i + 4) < list.size()) action.accept(list.get(i + 4));
+        }
+    }
+
+    public static <R> void x6(List<R> list, final Consumer<R> action) {
+        for (int i = 0; i < list.size(); i += 6) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+            if ((i + 3) < list.size()) action.accept(list.get(i + 3));
+            if ((i + 4) < list.size()) action.accept(list.get(i + 4));
+            if ((i + 5) < list.size()) action.accept(list.get(i + 5));
+        }
+    }
+
+    public static <R> void x7(List<R> list, final Consumer<R> action) {
+
+        for (int i = 0; i < list.size(); i += 7) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+            if ((i + 3) < list.size()) action.accept(list.get(i + 3));
+            if ((i + 4) < list.size()) action.accept(list.get(i + 4));
+            if ((i + 5) < list.size()) action.accept(list.get(i + 5));
+            if ((i + 6) < list.size()) action.accept(list.get(i + 6));
+        }
+    }
+
+    public static <R> void x8(List<R> list, final Consumer<R> action) {
+
+        for (int i = 0; i < list.size(); i += 8) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+            if ((i + 3) < list.size()) action.accept(list.get(i + 3));
+            if ((i + 4) < list.size()) action.accept(list.get(i + 4));
+            if ((i + 5) < list.size()) action.accept(list.get(i + 5));
+            if ((i + 6) < list.size()) action.accept(list.get(i + 6));
+            if ((i + 7) < list.size()) action.accept(list.get(i + 7));
+        }
+    }
+
+    public static <R> void x9(List<R> list, final Consumer<R> action) {
+
+        for (int i = 0; i < list.size(); i += 9) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+            if ((i + 3) < list.size()) action.accept(list.get(i + 3));
+            if ((i + 4) < list.size()) action.accept(list.get(i + 4));
+            if ((i + 5) < list.size()) action.accept(list.get(i + 5));
+            if ((i + 6) < list.size()) action.accept(list.get(i + 6));
+            if ((i + 7) < list.size()) action.accept(list.get(i + 7));
+            if ((i + 8) < list.size()) action.accept(list.get(i + 8));
+        }
+    }
+
+    public static <R> void x10(List<R> list, final Consumer<R> action) {
+
+        for (int i = 0; i < list.size(); i += 10) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+            if ((i + 3) < list.size()) action.accept(list.get(i + 3));
+            if ((i + 4) < list.size()) action.accept(list.get(i + 4));
+            if ((i + 5) < list.size()) action.accept(list.get(i + 5));
+            if ((i + 6) < list.size()) action.accept(list.get(i + 6));
+            if ((i + 7) < list.size()) action.accept(list.get(i + 7));
+            if ((i + 8) < list.size()) action.accept(list.get(i + 8));
+            if ((i + 9) < list.size()) action.accept(list.get(i + 9));
+        }
+    }
+
+
+    public static <R> void x11(List<R> list, final Consumer<R> action) {
+
+        for (int i = 0; i < list.size(); i += 11) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+            if ((i + 3) < list.size()) action.accept(list.get(i + 3));
+            if ((i + 4) < list.size()) action.accept(list.get(i + 4));
+            if ((i + 5) < list.size()) action.accept(list.get(i + 5));
+            if ((i + 6) < list.size()) action.accept(list.get(i + 6));
+            if ((i + 7) < list.size()) action.accept(list.get(i + 7));
+            if ((i + 8) < list.size()) action.accept(list.get(i + 8));
+            if ((i + 9) < list.size()) action.accept(list.get(i + 9));
+            if ((i + 10) < list.size()) action.accept(list.get(i + 10));
+        }
+    }
+
+    public static <R> void x12(List<R> list, final Consumer<R> action) {
+
+        for (int i = 0; i < list.size(); i += 12) {
+            action.accept(list.get(i));
+            if ((i + 1) < list.size()) action.accept(list.get(i + 1));
+            if ((i + 2) < list.size()) action.accept(list.get(i + 2));
+            if ((i + 3) < list.size()) action.accept(list.get(i + 3));
+            if ((i + 4) < list.size()) action.accept(list.get(i + 4));
+            if ((i + 5) < list.size()) action.accept(list.get(i + 5));
+            if ((i + 6) < list.size()) action.accept(list.get(i + 6));
+            if ((i + 7) < list.size()) action.accept(list.get(i + 7));
+            if ((i + 8) < list.size()) action.accept(list.get(i + 8));
+            if ((i + 9) < list.size()) action.accept(list.get(i + 9));
+            if ((i + 10) < list.size()) action.accept(list.get(i + 10));
+            if ((i + 11) < list.size()) action.accept(list.get(i + 11));
+        }
+    }
+
+    /**
+     * finds the largest number that the parameter is divisible by
+     *
+     * @param f
+     * @return
+     */
+    public static int gfac(int f) {
+        int x = f - 1;
+
+        while ((f % x) != 0) {
+            x--;
+        }
+        return x;
+    }
+
+}
---
+++ org.light32.pd.utils-0.0.1-SNAPSHOT/src/test/java/org/light32/pd/utils/UnrollerTest.java
@@ -0,0 +1,290 @@
+package org.light32.pd.utils;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unroller unit test
+ *
+ * @author jwhitt 9/14/16
+ */
+public class UnrollerTest {
+
+
+    @Test
+    public void testGfac() {
+        assertEquals(5, Unroller.gfac(10));
+        assertEquals(12, Unroller.gfac(24));
+        assertEquals(50, Unroller.gfac(100));
+        assertEquals(512, Unroller.gfac(1024));
+
+        // odd
+        assertEquals(11, Unroller.gfac(77));
+        assertEquals(111, Unroller.gfac(333));
+
+    }
+
+    @Test
+    public void testX2() {
+        List<String> input = getStringList(20);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x2(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX2_uneven() {
+        List<String> input = getStringList(15);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x2(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+
+    @Test
+    public void testX3() {
+        List<String> input = getStringList(45);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x3(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX3_uneven() {
+        List<String> input = getStringList(49);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x3(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX4() {
+        List<String> input = getStringList(48);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x4(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX4_uneven() {
+        List<String> input = getStringList(33);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x4(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX5() {
+        List<String> input = getStringList(55);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x5(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX5_uneven() {
+        List<String> input = getStringList(56);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x5(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX6() {
+        List<String> input = getStringList(72);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x6(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX6_uneven() {
+        List<String> input = getStringList(50);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x6(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX7() {
+        List<String> input = getStringList(49);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x7(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX7_uneven() {
+        List<String> input = getStringList(40);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x7(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX8() {
+        List<String> input = getStringList(64);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x8(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX8_uneven() {
+        List<String> input = getStringList(65);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x8(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX9() {
+        List<String> input = getStringList(81);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x9(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX9_uneven() {
+        List<String> input = getStringList(81);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x9(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX10() {
+        List<String> input = getStringList(100);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x10(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX10_uneven() {
+        List<String> input = getStringList(99);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x10(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX11() {
+        List<String> input = getStringList(121);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x11(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX11_uneven() {
+        List<String> input = getStringList(104);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x11(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX12() {
+        List<String> input = getStringList(144);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x12(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    @Test
+    public void testX12_uneven() {
+        List<String> input = getStringList(145);
+        List<String> outList = new ArrayList<>();
+
+        Unroller.x12(input, (s) -> {
+            outList.add(s + "processed");
+        });
+        assertTheList(outList);
+    }
+
+    ////
+    // helpers
+    ////
+    private void assertTheList(List<String> input) {
+        for (int x = 0; x < input.size(); x++) {
+            assertEquals("testString" + x + "processed", input.get(x));
+        }
+    }
+
+    private List<String> getStringList(int i) {
+        List<String> out = new ArrayList<>();
+        for (int x = 0; x <= i; x++) {
+            out.add("testString" + x);
+        }
+        return out;
+    }
+}
